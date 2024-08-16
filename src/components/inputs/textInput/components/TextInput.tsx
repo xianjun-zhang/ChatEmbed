095c4b477228fe1afbb6a44b496bb3cf1f8e4fac
xianjun-zhang <zxj.job@outlook.com>
2024-08-16 16:49:29 -0700

    Update readme and all url links

--- src/components/inputs/textInput/components/TextInput.tsx
+++ src/components/inputs/textInput/components/TextInput.tsx
@@ -29,7 +29,7 @@ type Props = {
 const defaultBackgroundColor = '#ffffff';
 const defaultTextColor = '#303235';
 // CDN link for default send sound
-const defaultSendSound = 'https://cdn.jsdelivr.net/gh/FlowiseAI/FlowiseChatEmbed@latest/src/assets/send_message.mp3';
+const defaultSendSound = 'https://cdn.jsdelivr.net/gh/xianjun-zhang/ChatEmbed@latest/src/assets/send_message.mp3';
 
 export const TextInput = (props: Props) => {
   const [inputValue, setInputValue] = createSignal(props.defaultValue ?? '');
